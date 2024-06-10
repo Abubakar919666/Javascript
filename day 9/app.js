075a7679fca6e520e464ec34d2c6f23a54c4c2bd
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-06-10 15:57:17 +0500

    update javascript

--- day 9/app.js
+++ day 9/app.js
@@ -35,6 +35,13 @@
 
 
 
-let userName =["haider","ghani","usman","ali"]
-userName = userName.map(fName => fName[0].toUpperCase()+fName.slice(1));
-console.log(userName);
+// let userName =["haider","ghani","usman","ali"]
+// userName = userName.map(fName => fName[0].toUpperCase()+fName.slice(1));
+// console.log(userName);
+
+
+
+let alNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 , 17, 18, 20 ];
+
+let divisibleby5and2 = alNum.filter(alNum => alNum % 5 === 0 && alNum % 2 === 0);
+console.log(divisibleby5and2);
