e3c4b4a2234af535c3ff3213f9d8fd9730fae281
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-07-06 15:14:53 +0500

    Variable are done

---
+++ day 1/README.md
@@ -0,0 +1,31 @@
+##  Variables and Data Types
+
+# Variables are Containers for Storing Data.
+    
+    JavaScript Variables can be declared in 4 ways:
+    Automatically
+    var
+    let
+    const
+
+# Note:
+
+
+    The var keyword was used in all JavaScript code from 1995 to 2015.
+
+    The let and const keywords were added to JavaScript in 2015.
+
+    The var keyword should only be used in code written for older browsers.
+
+
+# When to Used var, let, or const?
+
+1. Always declare variables
+
+2. Always use const if the value should not be changed
+
+3. Always use const if the type should not be changed (Arrays and Objects)
+
+4. Only use let if you can't use const
+
+5. Only use var if you must support old browsers.
--- day 1/app.js
+++ day 1/app.js
@@ -1,10 +1,51 @@
-// Variables and Data Types
+//  Variables
 
 
-alert("welcome")
-alert("welcome BECK")
-alert("welcome BK")
+// Automatically declared variables :
+
+    x = 3;
+    y = 7;
+    z = x + y;
+    console.log(z);
+
+// Var Variable are used as:
+    
+    var x = 5;
+    var y = 6;
+    var z = x + y;
+    console.log(z);
+
+// let Variable are used as:
+
+    let a = 8;
+    let b = 8;
+    let c = a + b;
+    console.log(c);
+
+// Const Variable are used as:
+    const k = 11;
+    const l = 4;
+    const m = k + l;
+    console.log(m);
 
 
 
+
+    //  MY introduction
+
+let firstName = "Muhammad";
+let lastName = "Abubakar";
+let age = 19;
+let isPakistan = true;
+
+
+let userIntro =
+    "Hello, My name is "
+     + firstName + " " 
+     + lastName + 
+     ". I am " + 
+     age 
+     + " " + "Year old.";
+
+    console.log(userIntro);
 
