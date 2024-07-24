b1ecebd1aa2478a27ec6c97c50d3a1e28a878349
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-07-25 01:58:30 +0500

    function start

--- day 4/README.md
+++ day 4/README.md
@@ -1,4 +1,9 @@
  # Learning Objectives
+- for loop
+- if condition inside for loop
+- for loop inside for loop
+- array inside for loop
+- keyword in loop (break and continue) 
 - Overview of Loops in JavaScript
 - Introduction to While Loop Syntax
 - Understanding While Loop Execution
--- day 4/app.js
+++ day 4/app.js
@@ -1,3 +1,68 @@
+// Loop ::
+    
+// if condition inside for loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+        if (element == 5) {
+            console.log("5 is best number");
+        }
+    console.log(element);
+
+}
+console.log(element);
+
+
+
+
+// for loop inside for loop
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(`Inner loop value ${j} and inner loop ${i}`);
+        console.log(i + '*' + j + ' = ' + i*j );
+    }
+    
+}
+
+
+
+
+// array inside for loop
+
+let myArray = ["flash", "batman", "superman"]
+console.log(myArray.length);
+    for (let index = 0; index < myArray.length; index++) {
+        const element = myArray[index];
+        console.log(element);
+
+    }
+
+
+
+
+// keyword in loop (break and continue)
+
+for (let index = 1; index <= 10; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        break
+    }
+    console.log(`Value of i is ${index}`);
+}
+
+for (let index = 1; index <= 10; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`Value of i is ${index}`);
+}
+
+
+
+
 // Understanding While Loop Execution
 let index = 0;
     while (index <= 10) {
@@ -6,14 +71,19 @@ let index = 0;
     }
 
 
+
+
 // Understanding while Loope through Arrays 
 
-let myArray = ["flash", "batman", "superman"];
+let myArray1 = ["flash", "batman", "superman"];
 let arr = 0;
-    while (arr < myArray.length) {
-        console.log(`Value is ${myArray[arr]}`);
+    while (arr < myArray1.length) {
+        console.log(`Value is ${myArray1[arr]}`);
         arr = arr + 1;
     }
+
+
+
 
 
 // Do-While Loops Overview:
