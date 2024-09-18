b2ce70f661ac0e30fc407bf8159e5393f8d2ae41
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-09-18 12:22:41 +0500

    update a dom file

--- day 6- objects/app.js
+++ day 6- objects/app.js
@@ -11,12 +11,12 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "abubakar",
+    "full name": "M.Abubakar",
     [mySym]: "mykey1",
     age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    location: "karachi",
+    email: "alirazaaliraza919666@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
@@ -26,9 +26,9 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
+JsUser.email = "abubakar@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "abubakar@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
